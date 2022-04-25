5bd3b9854edbc5f088f17c1a98b81da004204dd2
ripiobraido <93287807+ripiobraido@users.noreply.github.com>
2022-04-25 12:48:52 -0300

    Update resize.sh

--- resize.sh
+++ resize.sh
@@ -1,37 +1,4 @@
 #!/bin/bash
-
-# Specify the desired volume size in GiB as a command line argument. If not specified, default to 20 GiB.
-SIZE=${1:-20}
-
-# Get the ID of the environment host Amazon EC2 instance.
-INSTANCEID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
-
-# Get the ID of the Amazon EBS volume associated with the instance.
-VOLUMEID=$(aws ec2 describe-instances \
---instance-id $INSTANCEID \
---query "Reservations[0].Instances[0].BlockDeviceMappings[0].Ebs.VolumeId" \
---output text)
-
-echo Modify volume ${VOLUMEID}
-
-# Resize the EBS volume.
-aws ec2 modify-volume --volume-id $VOLUMEID --size $SIZE
-
-# Wait for the resize to finish.
-seconds=1
-
-while [ \
-    "$(aws ec2 describe-volumes-modifications \
-        --volume-id $VOLUMEID \
-        --filters Name=modification-state,Values="optimizing","completed" \
-        --query "length(VolumesModifications)"\
-        --output text)" != "1" ]; do
-    sleep 1
-    let seconds++
-done
-
-echo ${seconds} seconds to resize disk
-
 #Check if we're on an NVMe filesystem
 if [ $(readlink -f /dev/xvda) = "/dev/xvda" ]
 then
